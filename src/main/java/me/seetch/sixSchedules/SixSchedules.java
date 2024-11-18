c761bbb34c856c9a12959c42daa8b0cff68c9a25
Mihail Sergeevich <seetch@inbox.ru>
2024-11-18 22:11:43 +0300

    Fix placeholders registration

--- src/main/java/me/seetch/sixSchedules/SixSchedules.java
+++ src/main/java/me/seetch/sixSchedules/SixSchedules.java
@@ -2,6 +2,8 @@ package me.seetch.sixSchedules;
 
 import me.seetch.sixSchedules.command.SixSchedulesCommand;
 import me.seetch.sixSchedules.manager.ScheduleManager;
+import me.seetch.sixSchedules.placeholder.SchedulePlaceholder;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SixSchedules extends JavaPlugin {
@@ -12,6 +14,9 @@ public final class SixSchedules extends JavaPlugin {
         saveDefaultConfig();
         scheduleManager = new ScheduleManager(this);
         scheduleManager.loadEvents();
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            new SchedulePlaceholder(this, scheduleManager).register();
+        }
         getCommand("sixschedules").setExecutor(new SixSchedulesCommand(this));
     }
 
